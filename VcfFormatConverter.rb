431d3d61e5ae9e4e1ddd5f5587ada120a17a0c23
Razvan Florea <razvan.florea91@gmail.com>
2014-03-17 12:57:33 +0100

    Display alleles basis added

--- VcfFormatConverter.rb
+++ VcfFormatConverter.rb
@@ -3,17 +3,21 @@ require "java"
 java_import "org.broadinstitute.variant.vcf.VCFFileReader"
 java_import "net.sf.samtools.util.CloseableIterator"
 java_import "org.broadinstitute.variant.variantcontext.VariantContext"
-java_import "java.io.File"
 
-input = File.new(ARGV[0])
-
+input = java.io.File.new(ARGV[0])
 vcfFR = VCFFileReader.new(input, false)
 iterator = vcfFR.iterator
 
-nr = 0
+nr = 1
 while iterator.hasNext do
+	puts "SNP nr: #{nr}"
 	next_variant = iterator.next
 	alleles =  next_variant.getAlleles
+	alleles_it = alleles.iterator
+	while alleles_it.hasNext
+		puts "\tallele: #{alleles_it.next.getDisplayBases}"
+	end
+
 	nr = nr + 1
 end
 
